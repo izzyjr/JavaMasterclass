db36d74c9e87581f67d737d28057c67c65a87cda
Israel Mesa <israelmesa@Israels-MBP.home>
2017-12-28 21:50:30 -0500

    HelloWorld and Variables

---
+++ HelloWorld/src/com/israelmesa/Main.java
@@ -0,0 +1,29 @@
+package com.israelmesa;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Creating your first Java project
+        System.out.println("Hello, world!");
+
+        //What are Variables?
+        int myFirstNumber = 5;
+        int mySecNumber = (10 + 5) + (2 * 10);
+        int myThirdNumber = 12;
+        int myFourthNumber = 6;
+        int myFifthNumber = mySecNumber * 2;
+
+        int total = (myFirstNumber + myThirdNumber + myFourthNumber);
+        int myLastOne = (1000 - total);
+
+        System.out.println(myFirstNumber);
+        System.out.println(mySecNumber);
+
+        //String
+        System.out.println("MyFirstNumber");
+        System.out.println(total);
+        System.out.println(myFifthNumber);
+        System.out.println(myLastOne);
+
+    }
+}
