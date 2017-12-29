be191722bc6a04469fc01817b179d82837735b18
Israel Mesa <israelmesa@Israels-MBP.home>
2017-12-28 22:53:25 -0500

    Char Boolean

---
+++ CharAndBoolean/src/com/israelmesa/Main.java
@@ -0,0 +1,25 @@
+package com.israelmesa;
+
+public class Main {
+
+    public static void main(String[] args) {
+        char myChar = '\u00A9';
+        System.out.println(myChar);
+
+        //Boolean
+
+        boolean myBoolean = false;
+        boolean isMail = true;
+
+        //Challenge
+
+        char registered = '\u00AE';
+        System.out.println(registered);
+        System.out.println(myChar);
+
+        /*
+        only one letter or number (character)
+        unicode-table.com/en/#basic-latin
+        */
+    }
+}
