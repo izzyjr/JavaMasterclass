f55c798e9ea222b1301d9a3465c0d8cbfb900327
Israel Mesa <israelmesa@Israels-MBP.home>
2017-12-28 23:09:09 -0500

    Keywords Expressions

--- KeywordsAndExpressions/src/com/israelmesa/Main.java
+++ KeywordsAndExpressions/src/com/israelmesa/Main.java
@@ -7,7 +7,7 @@ public class Main {
         double kilometers = (100 * 1.609344);
 
         int highScore = 50;
-        if (highScore ==  50)
+        if (highScore == 50)
         {
             System.out.println("This is an expression");
         }
