d929f5f7dcca87fdf7587a44840b5b447999be03
Israel Mesa <israelmesa@Israels-MBP.home>
2017-12-30 13:12:34 -0500

    Methods Challenge

--- MethodsChallenge/src/com/israelmesa/Main.java
+++ MethodsChallenge/src/com/israelmesa/Main.java
@@ -21,11 +21,11 @@ public class Main {
         System.out.println(name + " Managed to get into position " + position + " on the high score table!!!");
     }
     public static int calculateHighScorePosition(int playerScore){
-        if (playerScore > 1000){
+        if (playerScore >= 1000){
             return 1;
-        } else if (playerScore > 500 && playerScore < 1000){
+        } else if (playerScore >= 500 && playerScore < 1000){
             return 2;
-        } else if (playerScore > 100 && playerScore < 500){
+        } else if (playerScore >= 100 && playerScore < 500){
             return 3;
         } else {
             return 4;
