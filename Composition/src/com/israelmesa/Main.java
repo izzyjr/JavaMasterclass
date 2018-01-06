f1245840457ef6fced6148e6707ce77fa1336d6e
Israel Mesa <israelmesa@Israels-MBP.home>
2018-01-05 22:30:24 -0500

    Composition

---
+++ Composition/src/com/israelmesa/Main.java
@@ -0,0 +1,19 @@
+package com.israelmesa;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Dimensions dimensions = new Dimensions(20, 20, 5);
+	    Case theCase = new Case("220B", "Dell", "240",  dimensions);
+
+	    Monitor monitor = new Monitor("27 inch Beast" ,"Acer", 27, new Resolution(2540, 1440));
+
+	    Motherboard motherboard = new Motherboard("Bj-200", "Asus",4, 6, "v2.44");
+
+	    PC thePc = new PC(theCase, monitor, motherboard);
+	    thePc.getMonitor().drawPixelAt(2000, 3000, "blue");
+	    thePc.getMotherboard().loadProgram("Windows 1.0");
+	    thePc.getTheCase().pressPowerButton();
+
+    }
+}
---
+++ Composition/src/com/israelmesa/Monitor.java
@@ -0,0 +1,35 @@
+package com.israelmesa;
+
+public class Monitor {
+    private String model;
+    private String manufacturer;
+    private int size;
+    private Resolution nativeResolotion;
+
+    public Monitor(String model, String manufacturer, int size, Resolution nativeResolotion) {
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.size = size;
+        this.nativeResolotion = nativeResolotion;
+    }
+
+    public void drawPixelAt(int x, int y, String color) {
+        System.out.println("Drawing pixel at " +  x + "," +  y + " in color " + color);
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public Resolution getNativeResolotion() {
+        return nativeResolotion;
+    }
+}
