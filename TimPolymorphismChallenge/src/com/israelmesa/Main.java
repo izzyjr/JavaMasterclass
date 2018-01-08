9fe7677d2f71a20d985a561c4e34344f8c3ac19b
Israel Mesa <israelmesa@Israels-MBP.home>
2018-01-07 21:31:41 -0500

    Tim's Polymorphism Challenge

---
+++ TimPolymorphismChallenge/src/com/israelmesa/Main.java
@@ -0,0 +1,137 @@
+package com.israelmesa;
+
+class Car {
+    private boolean engine;
+    private int cylinders;
+    private String name;
+    private int wheels;
+
+    public Car(int cylinders, String name) {
+        this.cylinders = cylinders;
+        this.name = name;
+        this.wheels = 4;
+        this.engine = true;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String startEngine() {
+        return "Car -> startEngine()";
+    }
+
+    public String accelerate() {
+        return "Car -> accelerate()";
+    }
+
+    public String brake() {
+        return "Car -> brake()";
+    }
+}
+
+class Mitsubishi extends Car {
+    public Mitsubishi(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return getClass().getSimpleName() + " -> startEngine()";
+    }
+
+    @Override
+    public String accelerate() {
+        return getClass().getSimpleName() + " -> accelerate()";
+    }
+
+    @Override
+    public String brake() {
+        return getClass().getSimpleName() + " -> brake()";
+    }
+}
+
+class Holden extends Car {
+    public Holden(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return "Holden -> startEngine()";
+    }
+
+    @Override
+    public String accelerate() {
+        return "Holden -> accelerate()";
+    }
+
+    @Override
+    public String brake() {
+        return "Holden -> brake()";
+    }
+}
+
+class Ford extends Car {
+    public Ford(int cylinders, String name) {
+        super(cylinders, name);
+    }
+
+    @Override
+    public String startEngine() {
+        return "Ford -> startEngine()";
+    }
+
+    @Override
+    public String accelerate() {
+        return "Ford -> accelerate()";
+    }
+
+    @Override
+    public String brake() {
+        return "Ford -> brake()";
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car car = new Car (8, "Base car");
+        System.out.println(car.startEngine());
+        System.out.println(car.accelerate());
+        System.out.println(car.brake());
+
+        System.out.println("...........................");
+
+        Holden holden = new Holden(6, "Holden Commodore");
+        System.out.println(holden.startEngine());
+        System.out.println(holden.accelerate());
+        System.out.println(holden.brake());
+        System.out.println(holden.getName());
+        System.out.println(holden.getCylinders());
+
+        System.out.println("...........................");
+
+        Ford ford = new Ford(6, "Ford Falcon");
+        System.out.println(ford.startEngine());
+        System.out.println(ford.accelerate());
+        System.out.println(ford.brake());
+        System.out.println(ford.getName());
+        System.out.println(ford.getCylinders());
+
+        System.out.println("...........................");
+
+        Mitsubishi mitsubish = new Mitsubishi(6, "Mitsubishi Outlander");
+        System.out.println(mitsubish.startEngine());
+        System.out.println(mitsubish.accelerate());
+        System.out.println(mitsubish.brake());
+        System.out.println(mitsubish.getName());
+        System.out.println(mitsubish.getCylinders());
+    }
+
+
+}
