3b9a8cd150103a926790aaf14ff2ef890649426b
Israel Mesa <mesa.israel50@gmail.com>
2018-01-27 12:25:57 -0500

    Update 2

--- LinkedListChallenge/src/com/israelmesa/Album.java
+++ LinkedListChallenge/src/com/israelmesa/Album.java
@@ -1,6 +1,7 @@
 package com.israelmesa;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Album {
     private String name;
@@ -30,6 +31,25 @@ public class Album {
         return null;
     }
 
+    public boolean addToPlaylist(int trackNumber, LinkedList<Song> playList) {
+        int index = trackNumber - 1;
+        if ((index > 0) && (index <= this.songs.size())) {
+            playList.add(this.songs.get(index));
+            return true;
+        }
+        System.out.println("This album does not have a track " + trackNumber);
+        return false;
+    }
+
+    public boolean addToPlaylist(String title, LinkedList<Song> playList) {
+        Song checkedSong = findSong(title);
+        if (checkedSong != null) {
+            playList.add(checkedSong);
+            return true;
+        }
+        System.out.println("The song " + title + " is not in this album");
+        return true;
+    }
 
 }
 
--- LinkedListChallenge/src/com/israelmesa/Main.java
+++ LinkedListChallenge/src/com/israelmesa/Main.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Main {
 
     private static Scanner scanner = new Scanner(System.in);
-    private Album ironWorks = new Album();
 
     public static void main(String[] args) {
 
