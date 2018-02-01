2a822673547bf8a81ab8737c767fc91fe01bf79b
Israel Mesa <mesa.israel50@gmail.com>
2018-01-31 21:36:46 -0500

    Setting up

---
+++ ListAndArrayListChallenge/src/com/israelmesa/Contacts.java
@@ -0,0 +1,11 @@
+package com.israelmesa;
+
+public class Contacts {
+    private String name;
+    private int phoneNumber;
+
+    public Contacts(String name, int phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+}
---
+++ ListAndArrayListChallenge/src/com/israelmesa/MobilePhone.java
@@ -0,0 +1,7 @@
+package com.israelmesa;
+
+import java.util.ArrayList;
+
+public class MobilePhone {
+    ArrayList<Contacts> contacts = new ArrayList<Contacts>();
+}
