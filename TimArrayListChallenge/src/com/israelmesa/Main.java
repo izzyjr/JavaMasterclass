43a8f685853c1cced8134d09474f2692a713edcd
Israel Mesa <mesa.israel50@gmail.com>
2018-02-13 22:17:31 -0500

    progress

--- TimArrayListChallenge/src/com/israelmesa/Main.java
+++ TimArrayListChallenge/src/com/israelmesa/Main.java
@@ -1,8 +1,77 @@
 package com.israelmesa;
 
+import java.util.Scanner;
+
 public class Main {
 
+    private static Scanner scanner = new Scanner(System.in);
+    private static MobilePhone mobilePhone = new MobilePhone("(401) 777-7777");
+
     public static void main(String[] args) {
-	// write your code here
+
+        boolean quit = false;
+        startPhone();
+        printActions();
+        while(!quit) {
+            System.out.println("\n Enter action: (6 to show available actions)");
+            int action = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (action) {
+                case 0:
+                    System.out.println("\n Shutting down...");
+                    quit = true;
+                    break;
+                case 1:
+                    mobilePhone.printContacts();
+                    break;
+                case 2:
+                    addNewContact();
+                    break;
+                case 3:
+                    updateContact();
+                    break;
+                case 4:
+                    removeContact();
+                    break;
+                case 5:
+                    queryContact();
+                    break;
+                case 6:
+                    printActions();
+                    break;
+            }
+        }
     }
+
+    private static void addNewContact() {
+        System.out.println("Enter new contact name: ");
+        String name = scanner.nextLine();
+        System.out.println("Enter phone number: ");
+        String phone = scanner.nextLine();
+        Contact newContact = Contact.createContact(name, phone);
+        if (mobilePhone.addNewContact(newContact)) {
+            System.out.println("New contact added. Name: " + name + " Phone: " + phone);
+        } else {
+            System.out.println("Can not add " + name + ", name is already on file.");
+        }
+    }
+
+    private static void startPhone() {
+        System.out.println("Starting phone...");
+    }
+
+    private static void printActions() {
+        System.out.println("\n Available actions: \npress");
+        System.out.println("0 - to shut down\n" +
+                            "1 - to print contacts\n" +
+                            "2 - to add a new contact\n" +
+                            "3 - to update an existing contact\n" +
+                            "4 - to remove an existing contact\n" +
+                            "5 - query if an existing contact exists\n" +
+                            "6 - to print a list of available actions.");
+        System.out.println("Choose your action: ");
+    }
+
+
 }
--- TimArrayListChallenge/src/com/israelmesa/MobilePhone.java
+++ TimArrayListChallenge/src/com/israelmesa/MobilePhone.java
@@ -6,7 +6,7 @@ public class MobilePhone {
     private String myNumber;
     private ArrayList<Contact> myContacts;
 
-    public MobilePhone(String myNumber, ArrayList<Contact> myContacts) {
+    public MobilePhone(String myNumber) {
         this.myNumber = myNumber;
         this.myContacts = new ArrayList<Contact>();
     }
@@ -32,6 +32,17 @@ public class MobilePhone {
         return true;
     }
 
+    public boolean removeContact(Contact contact) {
+        int foundPosition = findContact(contact);
+        if (foundPosition < 0) {
+            System.out.println(contact.getName() + " does not exist");
+            return false;
+        }
+        this.myContacts.remove(contact);
+        System.out.println(contact.getName() + " was deleted");
+        return true;
+    }
+
     private int findContact(Contact contact) {
         return this.myContacts.indexOf(contact);
     }
@@ -44,5 +55,20 @@ public class MobilePhone {
             }
         }
         return -1;
+    }
+
+    public String queryContact(Contact contact) {
+        if (findContact(contact) >= 0) {
+            return contact.getName();
+        }
+        return null;
+    }
+
+    public void printContacts() {
+        System.out.println("Contact List");
+        for(int i = 0; i < this.myContacts.size(); i++) {
+            System.out.println((i + 1) + "." + this.myContacts.get(i).getName() + ": "
+                                             + this.myContacts.get(i).getPhoneNumber());
+        }
     }
 }
